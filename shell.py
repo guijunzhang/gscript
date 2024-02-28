3fcd3b2c7eab236ee68588b80eda93b92034a1ac
guijunzhang <gzhang89@asu.edu>
2024-02-28 02:11:54 -0700

    Parser, Binary Operators, Unary Operators, Priority Operators (Parenthesis)

--- shell.py
+++ shell.py
@@ -1,7 +1,14 @@
 import gscript
 
+print("Enter 'exit' to exit terminal")
+
 while True:
 	text = input('gscript > ')
+
+	if (text == 'exit'): 
+		print('Done!')
+		break
+
 	result, error = gscript.run('stdin', text)
 
 	if error: print(error.as_string())
